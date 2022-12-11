e63cbcb1ad5f55792762a01a960886f4217567a3
pavan <chindukuripavan@gmail.com>
2022-12-11 11:00:02 +0530

    updating url

--- src/socket.ts
+++ src/socket.ts
@@ -7,5 +7,5 @@ export const initSocket = async () => {
 		timeout: 10000,
 		transports: ['websocket'],
 	};
-	return io(process.env.REACT_APP_BACKEND_URL || 'localhost:5000', options);
+	return io(process.env.REACT_APP_BACKEND_URL || '/', options);
 };
